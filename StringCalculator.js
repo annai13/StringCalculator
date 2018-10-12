80132f79d75b682f9dd1f2d5e54f1c75bf587627
Anna Birna <annai13@ru.is>
2018-10-12 13:03:41 +0000

    nr1

---
+++ StringCalculator.js
@@ -0,0 +1,15 @@
+function Add(numbers){
+
+ if (toString.call(numbers) !== "[object Array]")
+	return false;
+		var sum =  0;
+		for(var i=0;i<numbers.length;i++){
+			sum += Number(numbers[i]);
+
+		}
+
+		return sum;
+}
+console.log(Add([]));
+console.log(Add([1]));
+console.log(Add([1,2]));
