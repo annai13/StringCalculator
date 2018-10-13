4ee5ddba2e0d249c7655a4146045a0b2d9d5a0d1
Anna Birna <annai13@ru.is>
2018-10-13 13:54:30 +0000

    nr.5

--- StringCalculator.js
+++ StringCalculator.js
@@ -12,7 +12,11 @@ function Add(numbers){
 
 			if (0 > (numbers[i])){
 				throw "Negatives not allowed: " +(numbers[i]);
-				}
+			}
+
+			if((numbers[i]) > 1000){
+				continue;
+			}
 
 			sum += Number(numbers[i]);
 
@@ -24,8 +28,8 @@ function Add(numbers){
 
 console.log(Add([]));
 console.log(Add([1]));
-console.log(Add([1,2]));
-console.log(Add([1,'2\n',3,-4,-5]));
+console.log(Add([10000,2]));
+console.log(Add([1,'2\n',3,4,50000]));
 
 
 
