d3a8026a06e9bcd16681168b97b829d2cd2eb360
Anna Birna <annai13@ru.is>
2018-10-13 13:48:35 +0000

    nr.4

--- StringCalculator.js
+++ StringCalculator.js
@@ -9,16 +9,23 @@ function Add(numbers){
 			 return str.replace (/\n/g, ',');
 		}
 		for(var i=0;i<numbers.length;i++){
+
+			if (0 > (numbers[i])){
+				throw "Negatives not allowed: " +(numbers[i]);
+				}
+
 			sum += Number(numbers[i]);
 
 		}
 
 		return sum;
 }
+
+
 console.log(Add([]));
 console.log(Add([1]));
 console.log(Add([1,2]));
-console.log(Add([1,'2\n',3,4,5]));
+console.log(Add([1,'2\n',3,-4,-5]));
 
 
 
